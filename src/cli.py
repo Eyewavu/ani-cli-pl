354994446edba8d0e4a5bb8cd4f5594b08487ac8
Eyewavu <ew.music.yt@gmail.com>
2023-03-11 01:40:36 +0100

    pylint configs

--- src/cli.py
+++ src/cli.py
@@ -8,48 +8,48 @@ from src.video import get_video_link
 from src.utils import createMenu
 
 def cli():
-    parser =argparse.ArgumentParser("Anime CLI *PL*")
-    parser.add_argument("-s","--search",type=str)
+  parser =argparse.ArgumentParser("Anime CLI *PL*")
+  parser.add_argument("-s","--search",type=str)
 
-    args =parser.parse_args()
+  args =parser.parse_args()
 
-    series_found =False
-    series_link =""
+  series_found =False
+  series_link =""
 
-    if args.search is not None:
-      results =search( args.search )
-      list_items =list(map(lambda item: f"{Fore.GREEN}\"{item['title']}\" {Fore.BLUE}{item['episodes']}",results))
-      answer =createMenu(list_items)
+  if args.search is not None:
+    results =search( args.search )
+    list_items =list(map(lambda item: f"{Fore.GREEN}\"{item['title']}\" {Fore.BLUE}{item['episodes']}",results))
+    answer =createMenu(list_items)
 
-      if answer == 0: return
-      series_found =True
-      series_link =results[answer -1]['link']
+    if answer == 0: return
+    series_found =True
+    series_link =results[answer -1]['link']
 
-    episode_found =False
-    episode_link =""
+  episode_found =False
+  episode_link =""
 
-    if series_found:
-      results =get_episodes(series_link)
-      list_items =list(map(lambda item: f"{Fore.GREEN}\"{item['title']}\" {Fore.GREEN if item['avaiable'] else Fore.RED}{'✓' if item['avaiable'] else 'x'}",results))
-      answer =createMenu(list_items)
+  if series_found:
+    results =get_episodes(series_link)
+    list_items =list(map(lambda item: f"{Fore.GREEN}\"{item['title']}\" {Fore.GREEN if item['avaiable'] else Fore.RED}{'✓' if item['avaiable'] else 'x'}",results))
+    answer =createMenu(list_items)
 
-      if answer == 0: return
-      episode_found =True
-      episode_link = results[answer -1]['link']
-      
-    player_found =False
-    playerid =""
+    if answer == 0: return
+    episode_found =True
+    episode_link = results[answer -1]['link']
 
-    if episode_found:
-      results =get_player_ids(episode_link)
-      list_items =list(map(lambda item: f"{Fore.GREEN}\"{item['source']}\" {Fore.WHITE}{item['quality']} {Fore.RED}{item['dub_lang']} {Fore.WHITE}{item['sub_lang']}",results))
-      answer =createMenu(list_items)
+  player_found =False
+  playerid =""
 
-      if answer == 0: return
-      player_found =True
-      playerid =results[answer -1]['id']
+  if episode_found:
+    results =get_player_ids(episode_link)
+    list_items =list(map(lambda item: f"{Fore.GREEN}\"{item['source']}\" {Fore.WHITE}{item['quality']} {Fore.RED}{item['dub_lang']} {Fore.WHITE}{item['sub_lang']}",results))
+    answer =createMenu(list_items)
 
-    if player_found:
-      video_url =get_video_link(playerid)
-      print(f"video found at: {video_url}")
-      summon(["mpv",video_url],check=False)
+    if answer == 0: return
+    player_found =True
+    playerid =results[answer -1]['id']
+
+  if player_found:
+    video_url =get_video_link(playerid)
+    print(f"video found at: {video_url}")
+    summon(["mpv",video_url],check=False)
